07195dc16f3f963377e4a2015ebbf7d79d72d80d
Michael Specht <micha.specht@gmail.com>
2022-10-28 12:06:57 +0200

    yay

--- lib/neo4j_bolt.rb
+++ lib/neo4j_bolt.rb
@@ -660,7 +660,7 @@ module Neo4jBolt
 
         def disconnect()
             append_uint8(0xb1)
-            append_token(BOLT_GOODBYE)
+            append_token(BoltToken::BOLT_GOODBYE)
             flush()
             @state.set(ServerState::DEFUNCT)
         end
